21f5e344b48e0d7501e87e3f66b3cbd64ec291e3
christopher <gomas.christopher@gmail.com>
2021-10-12 10:26:10 +0000

    test

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,11 +1,21 @@
-```
 @extends('layout')
 
 @section('title', 'Mon template')
 
 @section('content')
-    <h1>Template personne A</h1>
+    <h1>Template personne B</h1>
     
-    <p>Le fichier de la personne A</p>
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+        </tbody>
+    </table>
+    
+    <p>Le fichier de la personne B après modifications</p>
 @endsection
-```
