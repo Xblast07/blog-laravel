cc6d457f8e7deb21d8355c78036396c0dfea6833
Xblast07 <antoine.rabaglia@live.fr>
2021-10-12 12:15:05 +0200

    Create template.blade.php

---
+++ resources/views/template.blade.php
@@ -0,0 +1,11 @@
+```
+@extends('layout')
+
+@section('title', 'Mon template')
+
+@section('content')
+    <h1>Template personne A</h1>
+    
+    <p>Le fichier de la personne A</p>
+@endsection
+```
